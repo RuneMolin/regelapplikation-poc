e691ef9fa6bd0feb12ed4204c1a05ac926179461
Rune Molin <rmo@nineconsult.dk>
2015-11-05 10:50:05 +0100

    Initial check in

---
+++ src/main/java/dk/skat/regelapp/MvcConfig.java
@@ -0,0 +1,21 @@
+package dk.skat.regelapp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author Rune Molin, rmo@nine.dk
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/editor").setViewName("editor");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
---
+++ src/main/java/dk/skat/regelapp/RegelApp.java
@@ -0,0 +1,15 @@
+package dk.skat.regelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Rune Molin, rmo@nine.dk
+ */
+@SpringBootApplication
+public class RegelApp {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(RegelApp.class, args);
+    }
+}
